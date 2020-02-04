39358d6599b564b85e6a2e950834f137f00a3be4
Joe Stanley <engineerjoe440@yahoo.com>
2020-02-03 20:29:51 -0800

    reorganized with platformio

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 *The Ultimate Heated Hog Water Management System*
 
 (c) Stanley Solutions - 2020
+
+
+**REFERENCES**
+- Hall-Effect Flow Sensor: https://www.openhacks.com/uploadsproductos/g3_4_water_flow_sensor_-_wiki.pdf
---
+++ src/main.cpp
@@ -0,0 +1,56 @@
+/*******************************************************************************
+Hog Water - A simple hog water heater control system by Stanley Solutions.
+(c) Stanley Solutions - 2020
+*******************************************************************************/
+
+// Includes
+#include <Arduino.h>
+#include "ESP8266WiFi.h"
+#include "ESPAsyncWebServer.h"
+
+// Define Debug Network Params
+const char* ssid = "InventorNet";
+const char* password =  "M!cr0c0ntr0l";
+
+// Define Release Network Params
+//const char* ssid = "BarnNet";
+//const char* password =  "$t@nl3yt3ch";
+
+// Define Web Server Port
+AsyncWebServer server(80);
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi..");
+  }
+
+  Serial.println(WiFi.localIP());
+
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(200, "text/plain", "You've Reached the Index!");
+  });
+
+  server.on("/hello", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(200, "text/plain", "Hello from ESP8266 server route");
+  });
+
+  server.on("/redirect/internal", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->redirect("/hello");
+  });
+
+  server.on("/redirect/external", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->redirect("https://techtutorialsx.com/");
+  });
+
+  server.begin();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
