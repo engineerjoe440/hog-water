d5e2279749b32a04da38045e2c6ed65e02234619
Joe Stanley <engineerjoe440@yahoo.com>
2020-02-03 23:39:45 -0800

    basic html templating and heartbeat

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@ Hog Water - A simple hog water heater control system by Stanley Solutions.
 *******************************************************************************/
 
 // Includes
+#include <FS.h>
 #include <stdio.h>
 #include <Arduino.h>
 #include <ArduinoJson.h>
@@ -21,9 +22,28 @@ const char* password =  "M!cr0c0ntr0l";
 // Define Web Server Port
 AsyncWebServer server(80);
 
+// Define Test Response
+String processor(const String& var)
+{
+
+  if(var == "PLACEHOLDER"){
+    return String(random(1,20));
+  }
+
+  return String();
+}
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
+
+  // Define LED Pin Mode
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  if(!SPIFFS.begin()){
+     Serial.println("An Error has occurred while mounting SPIFFS");
+     return;
+  }
 
   WiFi.begin(ssid, password);
 
@@ -38,8 +58,17 @@ void setup() {
     request->send(200, "text/plain", "You've Reached the Index!");
   });
 
+  server.on("/template", HTTP_GET, [](AsyncWebServerRequest *request){
+    Serial.println("This is the Template!");
+    request->send(SPIFFS, "/test.html", "text/html", false, processor);
+  });
+
   server.on("/hello", HTTP_GET, [](AsyncWebServerRequest *request){
     request->send(200, "text/plain", "Hello from ESP8266 server route");
+  });
+
+  server.on("/json", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(200, "text/plain", "{'test':'json-test'}");
   });
 
   server.on("/redirect/internal", HTTP_GET, [](AsyncWebServerRequest *request){
@@ -55,4 +84,8 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
+  digitalWrite(LED_BUILTIN, LOW); // Turn the LED on (Note that LOW is the voltage level)
+  delay(1000); // Wait for a second
+  digitalWrite(LED_BUILTIN, HIGH); // Turn the LED off by making the voltage HIGH
+  delay(1000); // Wait for two seconds
 }
